03a8eb92ad81a4fd18e377e76bb4e36cf8d3f141
Davin Glynn <glynnd@moravian.edu>
2023-04-19 11:05:23 -0400

    Adding gunicorn funcitonality

---
+++ conf/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/Users/glynndmoravian.edu/final_devops/Final-Project-Dev-Ops/.venv/bin/gunicorn'
+pythonpath = '/Users/glynndmoravian.edu/final_devops/Final-Project-Dev-Ops/django_website'
+bind = '0.0.0.0:5555'
+workers = 1
