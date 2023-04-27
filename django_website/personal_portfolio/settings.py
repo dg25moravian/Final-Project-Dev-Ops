693c10a81fbbb5438eb9649e2e973bb9a76737c5
Davin Glynn <glynnd@moravian.edu>
2023-04-27 08:40:41 -0400

    automatically adds ip to ALLOWED_HOSTS

--- django_website/personal_portfolio/settings.py
+++ django_website/personal_portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from requests import get
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-hnr28j6%s19i%vi10_1^2l7plvz-a*$26*gok7rciyi#byq%10
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['0.0.0.0','localhost','54.87.95.53']
+ip = get('https://api.ipify.org').content.decode('utf8')
+ALLOWED_HOSTS = ['0.0.0.0','localhost',ip]
 
 
 # Application definition
